cefff07b6e171f1454f73d15184a9be1e008179a
jayesh <jayesh85999@gmail.com>
2024-01-21 14:47:38 +0530

    number compare

---
+++ 18 jan/dictTest.py
@@ -0,0 +1,30 @@
+monthDictionary= {
+    "jan" : "january",
+    "feb" : "02",
+    "march" : "03",
+    "april" : "04",
+    "may" : "05"
+}
+
+
+# print(monthDictionary["jan"])
+print(monthDictionary)
+
+
+print(monthDictionary["march"])
+print(monthDictionary.get("april"))
+
+
+numberDictionary={
+
+
+    1: "one",
+    2:"two",
+    3:"three",
+    4:"four",
+    5:"five",
+    6:"six"
+
+}
+print(numberDictionary)
+print(numberDictionary[1])
