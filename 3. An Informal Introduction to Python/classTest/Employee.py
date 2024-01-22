583dd84eed268d627148b973f5bb1ca971333e27
Prashant Suryavanshi <PrashantSuryavanshi@fico.com>
2024-01-22 09:37:57 +0100

    more classes

--- 3. An Informal Introduction to Python/classTest/Employee.py
+++ 3. An Informal Introduction to Python/classTest/Employee.py
@@ -11,11 +11,29 @@ class Employee:
         
     
     def isMySalaryHigh(self):
-        if self.salary < 500:
+        if self.salary < 3000:
             print('No!!')
         else:
             print('Yes!!')
-  
-  
-  
+
+
+
+class EmployeeII:
+
+    id: 0
+    name: 'default'
+    salary: 0000
+
+    def __init__(self, id, name, salary):
+        self.id = id
+        self.name = name
+        self.salary = salary
+
+
+    def isMySalaryHigh(self):
+        if self.salary < 1500:
+            print('No!!')
+        else:
+            print('Yes!!')
+
   
--- 3. An Informal Introduction to Python/classTest/test.py
+++ 3. An Informal Introduction to Python/classTest/test.py
@@ -1,10 +1,15 @@
 from Employee import Employee
+from Employee import EmployeeII
 from simple import GFG
 
 instance = GFG()
 
 print(instance.add(15,5))
 
-shyam = Employee(1, 'shyam', 6000)
+shyam = Employee(1, 'shyam', 2500)
+
+shyam.isMySalaryHigh()
+
+shyam = EmployeeII(1, 'shyam', 2500)
 
 shyam.isMySalaryHigh()
